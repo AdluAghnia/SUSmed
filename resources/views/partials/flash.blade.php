214435f322958834ccec2dbd4b8384744bb33050
AdluAghnia <adluaghnia03@gmail.com>
2024-07-12 19:08:51 +0700

    Add
    flash error message

---
+++ resources/views/partials/flash.blade.php
@@ -0,0 +1,16 @@
+<!-- Flash Message -->
+@if (session('success'))
+    <div class="bg-green-500 text-white p-4 rounded mb-4">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="bg-red-500 text-white p-4 rounded mb-4">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
