5d90d4ad75a185f215bb459acecf1e7e2e92f0df
AdluAghnia <adluaghnia03@gmail.com>
2024-07-13 17:58:46 +0700

    Re Design the comment form

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,6 +1,6 @@
-<form hx-post="{{ route('comments.store', $post_id) }}" hx-swap="outerHTML" hx-target="#comments-list">
-    <textarea name="comment" id="comment" class="textarea textarea-primary textarea-lg w-full max-w-xs"
-        placeholder="Comment Here  ...."></textarea>
+<form class="flex" hx-post="{{ route('comments.store', $post_id) }}" hx-swap="outerHTML" hx-target="#comments-list">
+    <input name="comment" id="comment" type="text" placeholder="Type here"
+        class="input input-bordered input-primary w-full max-w-lg dark:bg-gray-700 mb-3 rounded border-2 border-indigo-500" />
     <x-input-error :messages="$errors->get('comment')" class="mt-2" />
-    <button class="btn btn-primary text-lg" type="submit"> >> </button>
+    <button class="justify-end ml-4 btn btn-primary text-lg" type="submit"> >> </button>
 </form>
