71a397cfa000aec7dde5e070c503d9e05f71b3bb
AdluAghnia <adluaghnia03@gmail.com>
2024-07-15 18:41:23 +0700

    error message/redirection

--- resources/views/comments/errors.blade.php
+++ resources/views/comments/errors.blade.php
@@ -1,7 +1,17 @@
 @if ($errors)
-    @foreach ($errors->all() as $error)
-        <p class="text-red-300">{{ $error }}</p>
-    @endforeach
+    <div role="alert" class="flex alert alert-error my-4">
+        <button class="justify-end" type="button" onclick="this.parentElement.style.display='none'">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block mr-2 shrink-0 stroke-current"
+                fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+        </button>
+
+        @foreach ($errors->all() as $error)
+            <span>{{ $error }}</span>
+        @endforeach
+    </div>
 @endif
 
 @include('comments.showAll', ['comments' => $comments])
